9bfd748aef78b1ba858d00297f576639732949c0
ambrosiorocha <ambrosiorocha2@gmail.com>
2025-11-11 23:57:06 -0300

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,1 +1,1 @@
-const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbw_Kmp3oS-yb8wybL_h6LJDSyflJ9KMVz3sURv-ffBMVdWAJmonDDQaSjHgeIM_XH76mQ/exec';
+const SCRIPT_URL = 'https://script.google.com/macros/s/AKfycbzX6cv5WlS6xepR7Myx0wVtsc3nSpS57nBlHSZDyLud0M8RLEGeEERd3WkNUjZN03rK/exec';
